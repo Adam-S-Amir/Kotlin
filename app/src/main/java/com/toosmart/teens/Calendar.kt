db4d18fe91351bde6f9b2b490d454ca43a72db41
Adam $. Amir <64651604+Adam-S-Amir@users.noreply.github.com>
2024-02-12 12:12:51 -0500

    Add Calendar button and activity

--- app/src/main/java/com/toosmart/teens/Calendar.kt
+++ app/src/main/java/com/toosmart/teens/Calendar.kt
@@ -1,10 +1,10 @@
 package com.toosmart.teens
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,9 +17,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.toosmart.teens.ui.theme.TooSmartTeensTheme
 
-class MainActivity : ComponentActivity() {
+class Calendar : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calendar)
+
+        val webView: WebView = findViewById(R.id.webView)
+        val url = "https://calendar.google.com/calendar/u/0/embed?src=henricotoosmart2start@gmail.com"
+        webView.loadUrl(url)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.cacheMode = WebSettings.LOAD_DEFAULT // Use default cache mode
     }
 }
--- app/src/main/java/com/toosmart/teens/flex.kt
+++ app/src/main/java/com/toosmart/teens/flex.kt
@@ -3,6 +3,7 @@ package com.toosmart.teens
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.activity.ComponentActivity
@@ -20,15 +21,69 @@ class flex : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.flex)
-        val button7 = findViewById<View>(R.id.fab1) as Button
-        val button8 = findViewById<View>(R.id.fab2) as Button
+        val button1 = findViewById<View>(R.id.page1_button1) as Button
+        val button2 = findViewById<View>(R.id.page1_button2) as Button
+        val button3 = findViewById<View>(R.id.page1_button3) as Button
+        val button4 = findViewById<View>(R.id.page1_button4) as Button
+        val button5 = findViewById<View>(R.id.page1_button5) as Button
+        val button6 = findViewById<View>(R.id.page1_button6) as Button
+        val button7 = findViewById<View>(R.id.page1_button7) as Button
+        val button8 = findViewById<View>(R.id.page1_button8) as Button
+        val button80 = findViewById<View>(R.id.fab1) as Button
+        val button90 = findViewById<View>(R.id.fab2) as Button
+        button1.setOnClickListener {
+            val url = "https://henricoprevention.org/youth-leadership/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val url = "https://henricoprevention.org/resources/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        button3.setOnClickListener {
+            val url = "https://henricoprevention.org/service-partners/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        button4.setOnClickListener {
+            val url = "https://www.instagram.com/henricotoosmart/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        button5.setOnClickListener {
+            val url = "https://www.youtube.com/channel/UCPoL8cFoxCdPlZFsa8YoLlQ"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        button6.setOnClickListener {
+            val url = ""
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
         button7.setOnClickListener {
-            val url = "tel:988"
+            val url = ""
             val intent = Intent(Intent.ACTION_VIEW)
             intent.setData(Uri.parse(url))
             startActivity(intent)
         }
         button8.setOnClickListener {
+            val intent = Intent(this, Calendar::class.java)
+            startActivity(intent)
+        }
+        button80.setOnClickListener {
+            val url = "tel:988"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        button90.setOnClickListener {
             val url = "https://988lifeline.org/chat/"
             val intent = Intent(Intent.ACTION_VIEW)
             intent.setData(Uri.parse(url))
